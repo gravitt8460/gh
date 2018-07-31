d60d7d9485e4cf2060b006145eae4a2146cb47e6
Karan <kacharekar@opencrowd.com>
2018-07-31 11:30:22 -0400

    gps

--- server/gpstest.py
+++ server/gpstest.py
@@ -17,6 +17,8 @@ while True:
 	if data[0]=="$GPRMC":
 		if data[2]=="A":
 			print("Latitude:" + str(convert(data[3],data[4])))
+			print(data[3])
+			print(data[4])
 			print("Longiitude:" + str(convert(data[5],data[6])))
 			
 			

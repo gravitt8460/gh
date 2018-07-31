96d1b3988fc9ac4303bfec5b2092ac9fdc36228e
Karan <kacharekar@opencrowd.com>
2018-07-31 10:21:50 -0400

    gps

--- server/gpstest.py
+++ server/gpstest.py
@@ -5,7 +5,7 @@ gps = serial.Serial("/dev/ttyACM0", baudrate=9600)
 while True:
 	line = gps.readline()
 	data = line.split(",")
-	if data[0]=="$GPMRC":
+	if data[0]=="$GPRMC":
 		if data[2]=="A":
 			print("Latitude:" + data[3])
 			print("Longiitude:" + data[5])
